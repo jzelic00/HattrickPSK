005a6de1c7a8af88929896b53d15b05cad3d277f
Josip Zelić <jzelic00@fesb.hr>
2019-11-11 04:37:59 +0100

    Adding admin controler

---
+++ HattrickPSK/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HattrickPSK.DataAcces;
+using HattrickPSK.Services;
+using HattrickPSK.Messages;
+using HattrickPSK.Models;
+using System;
+using HattrickPSK.RoleProvides;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+namespace HattrickPSK.Controllers
+{
+    [Authorize(Roles="Admin")]
+    public class AdminController : Controller
+    {
+        DAL dataAccess = new DAL();
+        
+        public ActionResult AdminHome()
+        {
+            
+            return View(dataAccess.getAllUsers());
+        }
+    }
+}
+    
